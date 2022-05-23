81d4f690d81e4b25be6f76c9a51b851cc0c36237
Arthur Ropke de Moraes <arthurropke@hotmail.com>
2022-05-22 22:09:04 -0300

    add login social with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,30 @@
-import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
+
 import styles from "./styles.module.scss";
 
 export const SignInButton = () => {
-  const isUserLoggeIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggeIn ? (
-    <button type="button" className={styles.signInButton}>
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Arthur Ropke
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
